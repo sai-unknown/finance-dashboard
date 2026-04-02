331ad47c10a5f18ca9a66f502574de39eb39e0fe
SAi <sandeeprathod455161@gmail.com>
2026-04-03 03:53:17 +0530

    Added Line Chart & mont_balc data

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,12 +1,20 @@
 import { transactions } from "../data/transactions";
+import { data } from "../data/monthly_balc";
+import {
+    LineChart
+    , Line,
+    XAxis,
+    YAxis,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts";
 
 function dashboard() {
 
     const income = transactions.filter((t) => t.type === "income").reduce((acc, t) => acc + t.amount, 0);
-
     const expenses = transactions.filter((t) => t.type === "expense").reduce((acc, t) => acc + t.amount, 0);
-
     const balance = income - expenses;
+
 
     return (
         <div>
@@ -27,7 +35,27 @@ function dashboard() {
                     <h3 className="text-gray-500">Expenses</h3>
                     <p className="text-2xl font-bold text-red-500">₹ {expenses.toLocaleString()}</p>
                 </div>
+            </div>
 
+            <div className="bg-white p-5 mt-5 rounded shadow">
+                {/* <div className="flex justify-between items-center mb-5">
+                    <h3 className="text-lg font-bold">Monthly Balance</h3>
+                    <select className="border rounded p-2">
+                        <option>2024</option>
+                        <option>2023</option>
+                        <option>2022</option>
+                    </select>
+                </div> */}
+                <h3 className="mb-3 font-bold" flex justify-center> Balance Over Time </h3>
+
+                <ResponsiveContainer width="100%" height={300}>
+                    <LineChart data={data}>
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Line type="monotone" dataKey="balance" stroke="#8884d8" />
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
         </div>
     );
