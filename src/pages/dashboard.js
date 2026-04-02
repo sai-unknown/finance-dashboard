c85cf7245642f4b01b904a0a4de77a426fdf0a04
SAi <sandeeprathod455161@gmail.com>
2026-04-03 02:40:48 +0530

    Added Static dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,27 @@
 function dashboard() {
-    return <h2>Dashboard Page</h2>;
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-5">Dashboard</h2>
+
+            <div className="grid grid-cols-3 gap-5">
+                <div className="bg-white shadow p-5">
+                    <h3 className="text-gray-500">Balance</h3>
+                    <p className="text-2xl font-bold">₹ 50,000</p>
+                </div>
+
+                <div className="bg-white shadow p-5 rounded">
+                    <h3 className="text-gray-500">Income</h3>
+                    <p className="text-2xl font-bold text-green-500">₹ 70,000</p>
+                </div>
+
+                <div className="bg-white shadow p-5 rounded">
+                    <h3 className="text-gray-500">Expenses</h3>
+                    <p className="text-2xl font-bold text-red-500">₹ 20,000</p>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default dashboard;
