666b9db7a5305825788243b8c3cf0c31515ccc63
SAi <sandeeprathod455161@gmail.com>
2026-04-05 21:22:47 +0530

    Updated insights

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import Header from "./components/header";
 import Dashboard from "./pages/dashboard";
 import Transaction from "./pages/transactionsPage";
 import Insights from "./pages/insights";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
   return (
     <Router>
       <div className="flex bg-gray-100 min-h-screen">
         <Sidebar />
-
         <div className="flex-1">
           <Header />
-
-          <div className="p-5">
+          <div className="flex-1 md:ml-64 p-4 md:p-6">
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/transactions" element={<Transaction />} />
@@ -24,7 +33,26 @@ function App() {
         </div>
       </div>
     </Router>
+
   );
 }
 
 export default App;
+
+  // return (
+  //   <Router>
+  //     <div className="flex bg-gray-100 min-h-screen">
+  //       <Sidebar />
+  //       <div className="flex-1">
+  //         <Header />
+  //         <div className="p-5">
+  //           <Routes>
+  //             <Route path="/" element={<Dashboard />} />
+  //             <Route path="/transactions" element={<Transaction />} />
+  //             <Route path="/insights" element={<Insights />} />
+  //           </Routes>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </Router>
+  // );
